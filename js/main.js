4b07075126338c36c29ec668f5f1a2d9c586a963
Maxim <kewin.rrrr@gmail.com>
2020-03-31 12:34:00 +0300

    feat: modal and modal-thanks

--- js/main.js
+++ js/main.js
@@ -1,9 +1,93 @@
 $(document).ready(function () {
+  // Бургер-меню
   var buttonWrapper = $('.button-wrapper'),
       wrapper = $('.wrapper');
       
   buttonWrapper.on('click', () => {
     wrapper.toggleClass('wrapper--active');
+  });
+
+  // Получение элементов
+  var modal = $('.modal'),                    //Модальное окно
+      modalBtn = $('[data-toggle=modal]');    // все элементы вызывающие модальное окно
+
+  // Вункция вызова модального окна
+  modalBtn.on('click', () => {
+    modal.toggleClass('modal--visible');
+  });
+      
+  // Вункция закрытия модального окна нажатием на крестик или на поле вокруг модального окна
+  modal.on('click', () => {
+    if (event.target.className === "modal modal--visible") {
+
+      modal.removeClass('modal--visible');
+
+    };
+  });
+
+  window.addEventListener("keydown", function(event){
+    if (event.keyCode == 27) {
+      modal.removeClass('modal--visible');
+      modalThanks.removeClass('modal--visible');
+    }
+  }, true);
+
+  // modal-thanks
+  var modalThanks = $('.modal-thanks');                    // Модальное окно
+        
+  // Вункция закрытия модального окна нажатием на крестик или на поле вокруг модального окна
+  modalThanks.on('click', () => {
+    if (event.target.className === "modal-thanks modal--visible") {
+
+      modalThanks.removeClass('modal--visible');
+
+    };
+  });
+
+  // Маска для номера телефона
+  $('[type=tel]').mask('+7 (000) 000-00-00');
+
+// Валидация форм
+  // Модальное окно
+  $('.modal__form').validate({
+    rules: {
+      modalName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      modalPhone: {
+        required: true,
+        minlength: 18,
+      }
+    },
+    errorClass: "invalid",
+    
+    messages: {
+      modalName: {
+        required: "Имя обязательно",
+        minlength: "Длина имени 2-15 символов",
+        maxlength: "Длина имени 2-15 символов"
+      },
+      modalPhone:{
+        required: "Телефон обязательно",
+        minlength: "Введите телефон полностью",
+      }
+    },
+    submitHandler: function(form) {
+      $.ajax({
+        type: "POST",
+        url: "send.php",
+        data: $(form).serialize(),
+        success: function (response) {
+          console.log('Ajax сработал, ответ с сервера', response);
+          $(form)[0].reset();
+          modal.removeClass('modal--visible');
+          modalThanks.toggleClass('modal--visible');
+          //ym(61250854,'reachGoal','form');
+        }
+      });
+    }
   });
 
 
